87f35619d05dc15d8c33b6066aad3ab5b2e57bbb
Maaam12 <suneater512@gmail.com>
2022-08-09 16:26:45 +0700

    feat [CUA] whatsapppage

---
+++ lib/info.dart
@@ -0,0 +1,94 @@
+const info = [
+  {
+    'name': 'Artoria ',
+    'message': 'Lets go Master!',
+    'time': '15.00',
+    'profilePic':
+        'https://static.wikia.nocookie.net/fategrandorder/images/2/27/AF0284.png/revision/latest?cb=20210402130029',
+  },
+  {
+    'name': 'Morgan',
+    'message': 'Merlin doko?',
+    'time': '14.25',
+    'profilePic':
+        'https://th.bing.com/th/id/OIP.7jhA3SiZGcD4BKcmIpfPMgHaJQ?pid=ImgDet&rs=1',
+  },
+  {
+    'name': 'Merlin',
+    'message': 'Im gonna die.',
+    'time': '13.03',
+    'profilePic':
+        'https://static.wikia.nocookie.net/fategrandorder/images/e/ed/AF0150%282021%29.png/revision/latest?cb=20210402131422',
+  },
+  {
+    'name': 'Sussy Mothman',
+    'message': 'Lemme kill em',
+    'time': '12.06',
+    'profilePic':
+        'https://th.bing.com/th/id/OIP.9sHaPfVwMW6oouNOwNvPWgHaIU?pid=ImgDet&rs=1',
+  },
+  {
+    'name': 'Mash',
+    'message': 'Did u eat ur breakfast Senpai?',
+    'time': '10.00',
+    'profilePic':
+        'https://64.media.tumblr.com/4465d2c65f0effae7cdc335ea6683dfd/9500843c840b1027-d3/s2048x3072/ce924853390e823629095e2541673bc2133a8833.png',
+  },
+  {
+    'name': 'Habetrot',
+    'message': 'Yabe, i forgot to turn off my stove',
+    'time': '09.53',
+    'profilePic':
+        'https://64.media.tumblr.com/1489a4d02b086ebb3a212556f00ce93b/9500843c840b1027-e0/s2048x3072/41ceedc9dc78666b417d6b41438953b4117e719f.png',
+  },
+  {
+    'name': 'Melusine',
+    'message': 'Stop treating me like a kid!!!',
+    'time': 'Kemarin',
+    'profilePic':
+        'https://i.ytimg.com/vi/lHIiF-Kogkk/hqdefault.jpg',
+  },
+  {
+    'name': 'Barghest',
+    'message': 'Im beeg',
+    'time': 'Kemarin',
+    'profilePic':
+        'https://pbs.twimg.com/media/E6RAy18UcAoTeJS.jpg',
+  },
+  {
+    'name': 'Baobhan Sith',
+    'message': 'Blood or strawberry juice?',
+    'time': '07/08/2022',
+    'profilePic':
+        'https://th.bing.com/th/id/OIP.IyqTasP79ksR66Q5uKsZeQHaEK?pid=ImgDet&rs=1',
+  },
+  {
+    'name': 'Percival',
+    'message': 'Lets collect some materials!',
+    'time': '07/08/2022',
+    'profilePic':
+        'https://i.ytimg.com/vi/cs-aCxZoZLQ/maxresdefault.jpg',
+  },
+];
+
+const messages = [
+  {"isMe": false, "text": "AAAAAAAA", "time": "10.00"},
+  {"isMe": true, "text": "AAAAAAAAAAAAA", "time": "11.00"},
+  {"isMe": false, "text": "AAAAAAAAAAAAAAAAAAA", "time": "11.01"},
+  {
+    "isMe": false,
+    "text": "Now wat?",
+    "time": "11.01"
+  },
+  {"isMe": true, "text": "Touch some grass", "time": "11.03"},
+  {
+    "isMe": false,
+    "text": "No, u touch some grass",
+    "time": "11.04"
+  },
+  {
+    "isMe": true,
+    "text": "Im so bored rn",
+    "time": "11.05"
+  },
+];
---
+++ lib/widgets/web_chat_appbar.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp_ui/colors.dart';
+
+class ChatAppBar extends StatelessWidget {
+  const ChatAppBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.077,
+      width: MediaQuery.of(context).size.width * 0.75,
+      padding: const EdgeInsets.all(10.0),
+      color: webAppBarColor,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              const CircleAvatar(
+                backgroundImage: NetworkImage(
+                  'https://static.wikia.nocookie.net/fategrandorder/images/2/27/AF0284.png/revision/latest?cb=20210402130029',
+                ),
+                radius: 20,
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.01,
+              ),
+              const Text(
+                'Artoria',
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.search, color: Colors.grey),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.more_vert, color: Colors.grey),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
